b821b8f7f799b56b4d9630b4245481d064e56a2b
GitHub Action <action@github.com>
2026-04-13 02:40:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/ryanxie0827/easytemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ryanxie0827/easytemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ryanxie0827.easytemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ryanxie0827.easytemplate.MyBundle
+import com.github.ryanxie0827.easytemplate.services.MyProjectService
 import javax.swing.JButton
 
 
